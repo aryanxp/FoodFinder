ee689c14bbdee1de3c3b46483c0855191862754e
Aryan Pitliya <38190250+aryanxp@users.noreply.github.com>
2021-12-15 18:07:25 +0530

    formatting changed

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -6,7 +6,7 @@ import star from '../logo.svg'
 import { Control, Errors, LocalForm } from "react-redux-form";
 import { Loading } from "./LoadingComponent";
 import { baseUrl } from "../shared/baseUrl";
-import {FadeTransform, Fade, Stagger} from 'react-animation-components'
+import { FadeTransform, Fade, Stagger } from 'react-animation-components'
 
 const minLength = (len) => (val) => val && (val.length >= len)
 const maxLength = (len) => (val) => !(val) || (val.length <= len)
@@ -98,15 +98,15 @@ function RenderDish({ dish }) {
     <div className="col-sm col-md-5 m-1">
       <FadeTransform in
         transformProps={{
-          exitTransform:'scale(0.5) translateY(-50%)'
+          exitTransform: 'scale(0.5) translateY(-50%)'
         }}>
-          <Card>
-            <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name} />
-            <CardBody>
-              <CardTitle>{dish.name}</CardTitle>
-              <CardText>{dish.description}</CardText>
-            </CardBody>
-          </Card>
+        <Card>
+          <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name} />
+          <CardBody>
+            <CardTitle>{dish.name}</CardTitle>
+            <CardText>{dish.description}</CardText>
+          </CardBody>
+        </Card>
       </FadeTransform>
     </div>
   );
@@ -122,19 +122,19 @@ function RenderComments({ comments, postComment, dishId }) {
               <Fade in>
                 <li key={comment.id}>
                   <p>(<img src={star} alt="" />
-                  {comment.rating})
-                  {comment.comment}</p>
+                    {comment.rating})
+                    {comment.comment}</p>
                   <p>-- {comment.author} ,{" "}
-                  {new Intl.DateTimeFormat("en-US", {
-                    year: "numeric",
-                    month: "short",
-                    day: "2-digit",
-                  }).format(new Date(Date.parse(comment.date)))}</p>
+                    {new Intl.DateTimeFormat("en-US", {
+                      year: "numeric",
+                      month: "short",
+                      day: "2-digit",
+                    }).format(new Date(Date.parse(comment.date)))}</p>
                 </li>
               </Fade>
             );
           })}
-      </Stagger>
+        </Stagger>
       </ul>
       <CommentForm dishId={dishId} postComment={postComment} />
     </div>
--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle } from 'reactstrap';
 import { baseUrl } from '../shared/baseUrl';
 import { Loading } from './LoadingComponent';
-import {FadeTransform} from 'react-animation-components'
+import { FadeTransform } from 'react-animation-components'
 
 function RenderCard({ item, isLoading, errMess }) {
   if (isLoading) {
@@ -17,18 +17,18 @@ function RenderCard({ item, isLoading, errMess }) {
   }
   else
     return (
-      <FadeTransform in 
+      <FadeTransform in
         tranformProps={{
           exitTransform: 'scale(0.5) tanslateY(-50%)'
         }}>
-      <Card>
-        <CardImg src={baseUrl + item.image} alt={item.name} />
-        <CardBody>
-          <CardTitle>{item.name}</CardTitle>
-          {item.designation ? <CardSubtitle>{item.designation}</CardSubtitle> : null}
-          <CardText>{item.description}</CardText>
-        </CardBody>
-      </Card>
+        <Card>
+          <CardImg src={baseUrl + item.image} alt={item.name} />
+          <CardBody>
+            <CardTitle>{item.name}</CardTitle>
+            {item.designation ? <CardSubtitle>{item.designation}</CardSubtitle> : null}
+            <CardText>{item.description}</CardText>
+          </CardBody>
+        </Card>
       </FadeTransform>
     );
 }
@@ -41,7 +41,7 @@ function Home(props) {
           <RenderCard item={props.dish} isLoading={props.dishesLoading} errMess={props.dishesErrMess} />
         </div>
         <div className="col-12 col-md m-1">
-          <RenderCard item={props.promotion} isLoading={props.promosLoading} errMess={props.promosErrMess}/>
+          <RenderCard item={props.promotion} isLoading={props.promosLoading} errMess={props.promosErrMess} />
         </div>
         <div className="col-12 col-md m-1">
           <RenderCard item={props.leader} />
